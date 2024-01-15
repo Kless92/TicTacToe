686dfb9b972743f1f0f7f53fe5151d564f9325c2
Spencer Klessens <klessens92@gmail.com>
2024-01-15 16:29:51 -0600

    and win conditions added and draw condition

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,4 @@
-from TicTacToeBorder import create_bord, check_turn
+from TicTacToeBorder import create_bord, check_turn, check_win
 import os
 setup = {1 : '·', 2 : '·', 3 : '·', 4 : '·', 5 : '·', 6 : '·', 7 : '·', 8 : '·', 9 : '·'}
 #class TicTacToeBord():
@@ -6,11 +6,16 @@ setup = {1 : '·', 2 : '·', 3 : '·', 4 : '·', 5 : '·', 6 : '·', 7 : '·', 8
 
 playing  = True
 turn = 0 
+prev_turn = -1
+complete  = False
 
 while playing:
     #Below is Clear operator
     os.system('cls' if os.name== 'nt' else 'clear')
     create_bord(setup)
+    if prev_turn == turn:
+        print('Invalid spot selected!')
+    prev_turn = turn
     print("Player "+ str((turn % 2) + 1) + "'s turn:\nPick your spot or press q to quite!")
     choice = input()
     if choice == 'q':
@@ -21,3 +26,22 @@ while playing:
         if not setup[int(choice)] in {'X', 'O'}:
             turn += 1
             setup[int(choice)] = check_turn(turn)
+    #Check if P1 or P2 has won
+    print('Near check win')
+    if check_win(setup):
+        playing, complete = False, True
+    #Checks if P1 or P2 has Draw
+    print('Near turn greater then 8')
+    if turn > 8:
+        playing = False
+
+os.system('cls' if os.name== 'nt' else 'clear')
+create_bord(setup)
+
+if complete:
+    if check_turn(turn) == 'X':
+        print('Player 1 Wins!')
+    else:
+        print('Player 2 Wins!')
+else:
+    print('This game is a draw!')
--- TicTacToeBorder.py
+++ TicTacToeBorder.py
@@ -17,3 +17,21 @@ def check_turn(turn):
         return 'O'
     else:
         return 'X'
+    
+def check_win(input):
+    if (input[1] == 'X' and input[2] == 'X' and input[3] == 'X' or input[1] == 'O' and input[2] == 'O' and input[3] == 'O') \
+     or (input[4] == 'X' and input[5] == 'X' and input[6] == 'X' or input[4] == 'O' and input[5] == 'O' and input[6] == 'O')  \
+     or (input[7] == 'X' and input[8] == 'X' and input[9] == 'X' or input[7] == 'O' and input[8] == 'O' and input[9] == 'O') :
+        print('Row')
+        return True
+    elif  (input[1] == 'X' and input[4] == 'X' and input[7] == 'X' or input[1] == 'O' and input[4] == 'O' and input[7] == 'O') \
+     or (input[2] == 'X' and input[5] == 'X' and input[8] == 'X' or input[2] == 'O' and input[5] == 'O' and input[8] == 'O')  \
+     or (input[3] == 'X' and input[6] == 'X' and input[9] == 'X' or input[3] == 'O' and input[6] == 'O' and input[9] == 'O') :
+        print('Col')
+        return True
+    elif  (input[1] == 'X' and input[5] == 'X' and input[9] == 'X' or input[1] == 'O' and input[5] == 'O' and input[9] == 'O') \
+     or (input[3] == 'X' and input[5] == 'X' and input[7] == 'X' or input[3] == 'O' and input[5] == 'O' and input[7] == 'O'):
+        print('Diangle')
+        return True
+    else:
+        return False
